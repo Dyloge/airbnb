63931ce7c730e5a5a44356d00151f51d258c5358
Dyloge <43300757+Dyloge@users.noreply.github.com>
2022-01-07 11:17:48 +0100

    responsive design of SubFooter( Footer RD done)

--- components/subcomponents/SubFooter.jsx
+++ components/subcomponents/SubFooter.jsx
@@ -2,7 +2,7 @@ const SubFooter = ({ title, items, key }) => {
   return (
     <div className='ml-10'>
       <div className=' font-semibold text-xl my-4'>{title}</div>
-      <div className=' grid grid-rows-2 grid-cols-3  gap-5 mb-10 text-xl cursor-pointer'>
+      <div className=' md:grid md:grid-rows-2 md:grid-cols-3  md:gap-5 mb-10 text-xl cursor-pointer sm:flex sm:flex-col'>
         {items.map((item) => (
           <div className=' w-max hover:underline' key={key}>
             {item}
