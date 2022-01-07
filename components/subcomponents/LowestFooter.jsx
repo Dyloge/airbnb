5ed520597174afd15b6614fe833c24e662a1457d
Dyloge <43300757+Dyloge@users.noreply.github.com>
2022-01-07 11:07:28 +0100

    responsive design of LowestFooter

--- components/subcomponents/LowestFooter.jsx
+++ components/subcomponents/LowestFooter.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const LowestFooter = () => {
   return (
     <div>
-      <div className='flex  justify-center my-10'>
+      <div className='flex  md:justify-center my-6 sm:justify-start sm:ml-4'>
         <div className='w-40 underline flex justify-center text-xl font-semibold cursor-pointer'>
           <Image src='/img/language.svg' width='18' height='18' alt='' />
           <div>English(US)</div>
@@ -11,16 +11,16 @@ const LowestFooter = () => {
         <div className='text-xl cursor-pointer'>
           zł <span className='underline font-semibold'>PLN</span>
         </div>
-        <div className=' w-56 flex justify-evenly'>
+        <div className=' w-56 flex justify-evenly sm:invisible md:visible'>
           <Image src='/img/facebook.svg' width='18' height='18' alt='' />
           <Image src='/img/instagram.svg' width='18' height='18' alt='' />
           <Image src='/img/twitter.svg' width='18' height='18' alt='' />
         </div>
       </div>
-      <div className='text-gray-700 text-xl flex justify-center'>
+      <div className='text-gray-700 text-xl flex md:justify-center sm:justify-start sm:ml-8'>
         &copy; 2022 Airbnb, Inc.
       </div>
-      <div className='flex  justify-center text-gray-700 cursor-pointer'>
+      <div className='flex  md:justify-center text-gray-700 cursor-pointer sm:justify-start sm:ml-8'>
         <div className="text-xl  hover:underline after:content-['.'] after:mx-2">
           Privacy
         </div>
